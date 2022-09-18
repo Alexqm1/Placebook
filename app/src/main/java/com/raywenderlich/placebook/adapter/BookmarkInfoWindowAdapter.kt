47434acd225522e2cdbf48e978edd74014f41d58
Alexqm1 <105534648+Alexqm1@users.noreply.github.com>
2022-09-17 22:10:03 -0400

    chapters 17-19

--- app/src/main/java/com/raywenderlich/placebook/adapter/BookmarkInfoWindowAdapter.kt
+++ app/src/main/java/com/raywenderlich/placebook/adapter/BookmarkInfoWindowAdapter.kt
@@ -1,16 +1,16 @@
 package com.raywenderlich.placebook.adapter
 
 import android.app.Activity
-import android.graphics.Bitmap
 import android.view.View
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.Marker
 
 import com.raywenderlich.placebook.databinding.ContentBookmarkInfoBinding
 import com.raywenderlich.placebook.ui.MapsActivity
+import com.raywenderlich.placebook.viewmodel.MapsViewModel
 
 // 2
-class BookmarkInfoWindowAdapter(context: Activity) : GoogleMap.InfoWindowAdapter {
+class BookmarkInfoWindowAdapter(val context: Activity) : GoogleMap.InfoWindowAdapter {
     // 3
     private val binding = ContentBookmarkInfoBinding.inflate(context.layoutInflater)
     // 4
@@ -18,13 +18,37 @@ class BookmarkInfoWindowAdapter(context: Activity) : GoogleMap.InfoWindowAdapter
         // This function is required, but can return null if
         // not replacing the entire info window
         val imageView = binding.photo
-        imageView.setImageBitmap((marker.tag as MapsActivity.PlaceInfo).image)
+        when (marker.tag) {
+            // 1
+            is MapsActivity.PlaceInfo -> {
+                imageView.setImageBitmap(
+                    (marker.tag as MapsActivity.PlaceInfo).image)
+            }
+            // 2
+            is MapsViewModel.BookmarkView -> {
+                val bookMarkview = marker.tag as
+                        MapsViewModel.BookmarkView
+                imageView.setImageBitmap(bookMarkview.getImage(context))
+            }
+        }
         return null
     }
     // 5
     override fun getInfoContents(marker: Marker): View? {
         binding.title.text = marker.title ?: ""
         binding.phone.text = marker.snippet ?: ""
+        val imageView = binding.photo
+        when (marker.tag) {
+            // 1
+            is MapsActivity.PlaceInfo -> {
+                imageView.setImageBitmap((marker.tag as MapsActivity.PlaceInfo).image)
+            }
+            // 2
+            is MapsViewModel.BookmarkView -> {
+                val bookMarkview = marker.tag as
+                        MapsViewModel.BookmarkView
+                imageView.setImageBitmap(bookMarkview.getImage(context))            }
+        }
         return binding.root
     }
 }
--- app/src/main/java/com/raywenderlich/placebook/viewmodel/MapsViewModel.kt
+++ app/src/main/java/com/raywenderlich/placebook/viewmodel/MapsViewModel.kt
@@ -1,6 +1,7 @@
 package com.raywenderlich.placebook.viewmodel
 
 import android.app.Application
+import android.content.Context
 import android.graphics.Bitmap
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
@@ -11,13 +12,18 @@ import com.google.android.libraries.places.api.model.Place
 import com.raywenderlich.placebook.model.Bookmark
 import com.raywenderlich.placebook.repository.BookmarkRepo
 
+import com.raywenderlich.placebook.util.ImageUtils
+
 // 1
 class MapsViewModel(application: Application) :
     AndroidViewModel(application) {
     private val TAG = "MapsViewModel"
+
     // 2
     private val bookmarkRepo: BookmarkRepo = BookmarkRepo(
-        getApplication())
+        getApplication()
+    )
+
     // 3
     fun addBookmarkFromPlace(place: Place, image: Bitmap?) {
         // 4
@@ -29,37 +35,75 @@ class MapsViewModel(application: Application) :
         bookmark.phone = place.phoneNumber.toString()
         bookmark.address = place.address.toString()
         // 5
+        bookmark.category = getPlaceCategory(place)
         val newId = bookmarkRepo.addBookmark(bookmark)
+        image?.let { bookmark.setImage(it, getApplication()) }
         Log.i(TAG, "New bookmark $newId added to the database.")
     }
 
 
-    data class BookmarkMarkerView(
-        var id: Long? = null,
-        var location: LatLng = LatLng(0.0, 0.0)
-    )
-    private fun bookmarkToMarkerView(bookmark: Bookmark) =
-        BookmarkMarkerView(
-            bookmark.id,
-            LatLng(bookmark.latitude, bookmark.longitude))
-
-    fun getBookmarkMarkerViews() :
-            LiveData<List<BookmarkMarkerView>>? {
-        val bookmarks = null
-        if (bookmarks == null) {
-            mapBookmarksToMarkerView()
+    data class BookmarkView(
+        val id: Long? = null,
+        val location: LatLng = LatLng(0.0, 0.0),
+        val name: String = "",
+        val phone: String = "",
+        val categoryResourceId: Int? = null
+    ) {
+        fun getImage(context: Context) = id?.let {
+            ImageUtils.loadBitmapFromFile(context, Bookmark.generateImageFilename(it)
+            )
         }
-        return bookmarks
     }
 
-    private fun mapBookmarksToMarkerView() {
+    private fun bookmarkToBookmarkView(bookmark: Bookmark):
+            BookmarkView {
+        return BookmarkView(
+            bookmark.id,
+            LatLng(bookmark.latitude, bookmark.longitude),
+            bookmark.name,
+            bookmark.phone,
+            bookmarkRepo.getCategoryResourceId(bookmark.category))
+    }
+
+    private fun mapBookmarksToBookmarkView() {
         // 1
-        var bookmarks = Transformations.map(bookmarkRepo.allBookmarks)
+        bookmarks = Transformations.map(bookmarkRepo.allBookmarks)
         { repoBookmarks ->
-            // 2
+// 2
             repoBookmarks.map { bookmark ->
-                bookmarkToMarkerView(bookmark)
+                bookmarkToBookmarkView(bookmark)
             }
         }
     }
+    fun getBookmarkViews() : LiveData<List<BookmarkMarkerView>>? {
+        if (bookmarks == null) {
+            mapBookmarksToBookmarkView()
+        }
+        return bookmarks
+    }
+    private fun getPlaceCategory(place: Place): String {
+        // 1
+        var category = "Other"
+        val types = place.types
+        types?.let { placeTypes ->
+// 2
+            if (placeTypes.size > 0) {
+                // 3
+                val placeType = placeTypes[0]
+                category = bookmarkRepo.placeTypeToCategory(placeType)
+            }
+        }
+// 4
+        return category
+    }
+
+    fun addBookmark(latLng: LatLng) : Long? {
+        val bookmark = bookmarkRepo.createBookmark()
+        bookmark.name = "Untitled"
+        bookmark.longitude = latLng.longitude
+        bookmark.latitude = latLng.latitude
+        bookmark.category = "Other"
+        return bookmarkRepo.addBookmark(bookmark)
+    }
+
 }
